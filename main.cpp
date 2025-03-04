b8600415f447fb7e706e7210015e20081e404b6c
Rob Altenburg <rca@qrpc.com>
2025-03-04 12:00:20 -0500

    updated the gregory_newton function

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <sstream>
 
 
-
+// Given a sequence of numbers, calculate the differences in successive lines recursively
 std::vector<std::vector<double> > generateDifferences(const std::vector<double>& input) {
     std::vector<std::vector<double> > result;
     if (input.empty()) return result;
@@ -40,6 +40,63 @@ std::vector<std::vector<double> > generateDifferences(const std::vector<double>&
     }
 
     return result;
+}
+
+// Extract the first item from each sub-vector in a vector of vectors
+std::vector<double> getFirstItems(const std::vector<std::vector<double>>& input) {
+    std::vector<double> result;
+    for (const auto& subVector : input) {
+        if (!subVector.empty()) {
+            result.push_back(subVector[0]);
+        }
+    }
+    return result;
+}
+
+std::string gregoryNewton(std::vector<double> input) {
+    std::ostringstream result;  
+
+    for (size_t i = 0; i < input.size(); i++) {
+        if (i == 0) {
+            // First term (constant term)
+            result << input[i];
+        } else if (i == 1) {
+            // Special case for i=1, simplify (n)/1! to just n
+            result << " + ";
+            if (input[i] == 1) {
+                result << "n";
+            } else {
+                result << input[i] << " * n";
+            }
+        } else {
+            // Subsequent terms for i > 1
+            result << " + ";
+            
+            // Numerator part: (n)(n-1)(n-2)...(n-(i-1)) * input[i]
+            result << "(";
+            for (size_t j = 0; j < i; j++) {
+                if (j > 0) {
+                    result << " * ";
+                }
+                if (j == 0) {
+                    result << "n";
+                } else {
+                    result << "(n - " << j << ")";
+                }
+            }
+            if (i > 0) {
+                if (input[i] == 1) {
+                    result << ")";
+                } else {
+                    result << " * " << input[i] << ")";
+                }
+            }
+            
+            // Denominator part: i!
+            result << " / " << i << "!";
+        }
+    }
+    return result.str();
 }
 
 int main() {
@@ -103,6 +160,8 @@ int main() {
         }
         std::cout << std::endl;
     }
+
+    std::cout << "\nGregory Newton Formula: (maxima definition)\n f(n) := " << gregoryNewton(getFirstItems(differences)) << ";" << std::endl;
     
     return 0;
 }
